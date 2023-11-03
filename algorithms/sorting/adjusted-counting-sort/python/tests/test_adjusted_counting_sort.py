a9a2aef9a456dee4bac7515ce634034349ef8beb
Cre4T3Tiv3 <jlmoses@outlook.com>
2023-11-02 18:49:43 -0600

    update(algorithms): Revised commenting of all in-sopce modules and the readme directory structure.

--- algorithms/sorting/adjusted-counting-sort/python/tests/test_adjusted_counting_sort.py
+++ algorithms/sorting/adjusted-counting-sort/python/tests/test_adjusted_counting_sort.py
@@ -4,12 +4,13 @@ sys.path.append(
     "../../.."
 )  # To include the parent directories in the module search path
 
-from algorithms.sorting.adjusted_counting_sort.python.adjusted_counting_sort import (
-    adjusted_counting_sort,
-)
+# Import the function to test.
+from adjusted_counting_sort import adjusted_counting_sort
+
 import unittest
 
 
+# The parent class.
 class TestAdjustedCountingSort(unittest.TestCase):
     """
     Unit tests for the Adjusted Counting Sort Algorithm
@@ -19,26 +20,35 @@ class TestAdjustedCountingSort(unittest.TestCase):
         """
         Basic tests for the Adjusted Counting Sort
         """
+        # Test 1: Test for a list with no duplicates.
         arr = [4, 2, 2, 8, 3, 3, 1]
+        # The expected output.
         self.assertEqual(adjusted_counting_sort(arr), [1, 2, 2, 3, 3, 4, 8])
 
+        # Test 2: Test for a list with duplicates.
         arr = [9, 7, 5, 3, 1]
+        # The expected output.
         self.assertEqual(adjusted_counting_sort(arr), [1, 3, 5, 7, 9])
 
     def test_empty(self):
         """
         Test for an empty list
         """
+        # Test 1: Test for an empty list.
         arr = []
+        # The expected output.
         self.assertEqual(adjusted_counting_sort(arr), [])
 
     def test_single_element(self):
         """
         Test for a list with a single element
         """
+        # Test 1: Test for a list with a single element.
         arr = [5]
+        # The expected output.
         self.assertEqual(adjusted_counting_sort(arr), [5])
 
 
+# Run the unit tests.
 if __name__ == "__main__":
     unittest.main()
